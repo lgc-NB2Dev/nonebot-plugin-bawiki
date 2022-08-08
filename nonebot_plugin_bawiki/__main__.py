90767783eaa780b2b9c8515549a49715434d2e90
student_2333 <lgc2333@126.com>
2022-08-08 17:26:41 +0800

    feat: 日程表改为图片形式

--- nonebot_plugin_bawiki/__main__.py
+++ nonebot_plugin_bawiki/__main__.py
@@ -1,4 +1,8 @@
-from nonebot import on_command, logger
+from datetime import datetime
+from pathlib import Path
+
+import jinja2
+from nonebot import logger, on_command
 from nonebot.adapters.onebot.v11 import Message, MessageSegment
 from nonebot.internal.matcher import Matcher
 from nonebot.params import CommandArg
@@ -22,25 +26,38 @@ async def _(matcher: Matcher):
     if not ret:
         return await matcher.finish('没有获取到数据')
 
-    li = Message()
     for i in ret:
-        des = i["title"] + (f"\n{x}" if (x := i["description"]) else "")
-        des = des.replace('<br>', '')
-        li += (
-            f'{format_timestamp(i["begin_at"])} - {format_timestamp(i["end_at"])}\n'
-            f'{des}'
-        )
-
         if pic := i['picture']:
             if (not pic.startswith('https:')) and (not pic.startswith('http:')):
-                pic = 'https:' + pic
-            li += MessageSegment.image(pic)
+                i['picture'] = 'https:' + pic
 
-        li += '\n==============\n'
+        begin = i["begin_at"]
+        end = i["end_at"]
+        i['date'] = (
+            f'{format_timestamp(begin)} ~ {format_timestamp(end)}'
+        )
 
-    li.pop(-1)
+        time_remain = datetime.fromtimestamp(end) - datetime.now()
+        mm, ss = divmod(time_remain.seconds, 60)
+        hh, mm = divmod(mm, 60)
+        i['dd'] = time_remain.days or 0
+        i['hh'] = hh
+        i['mm'] = mm
+        i['ss'] = ss
 
-    await matcher.finish(li)
+    try:
+        html = jinja2.Template(
+            (Path(__file__).parent / 'res' / 'calender.html.jinja').read_text('utf-8')
+        ).render(info=ret)
+
+        async with get_new_page() as page:  # type:Page
+            await page.set_content(html, wait_until='networkidle')
+            pic = await(await page.query_selector('xpath=//div[@id="calendar-box"]')).screenshot()
+    except:
+        logger.exception(f'渲染或截取页面出错')
+        return await matcher.finish(f'渲染或截取页面出错，请检查后台输出')
+
+    await matcher.finish(MessageSegment.image(pic))
 
 
 stu_wiki = on_command('ba学生图鉴')
@@ -62,15 +79,14 @@ async def _(matcher: Matcher, arg: Message = CommandArg()):
         return await matcher.finish('没有获取到学生列表数据')
 
     if not (sid := ret.get(arg)):
-        return matcher.finish('未找到该学生')
+        return await matcher.finish('未找到该学生')
 
     url = f'https://ba.gamekee.com/{sid}.html'
     await matcher.send(f'请稍等，正在截取Wiki页面……\n{url}')
 
     try:
         async with get_new_page() as page:  # type:Page
-            await page.goto(url)
-            await page.wait_for_load_state('networkidle')
+            await page.goto(url, wait_until='networkidle', timeout=60)
 
             # 删掉header
             await page.add_script_tag(
--- nonebot_plugin_bawiki/data_source.py
+++ nonebot_plugin_bawiki/data_source.py
@@ -9,7 +9,7 @@ async def game_kee_request(url, **kwargs):
         async with s.get(
                 url,
                 headers={
-                    'game-id': '0',
+                    'game-id'   : '0',
                     'game-alias': 'ba'
                 },
                 **kwargs
@@ -28,7 +28,7 @@ async def get_calender():
             li: list = i['list']
 
             now = time.time()
-            li = [x for x in li if x['end_at'] >= now]
+            li = [x for x in li if (x['end_at'] >= now >= x['begin_at'])]
 
             li.sort(key=lambda x: x['end_at'])
             li.sort(key=lambda x: x['importance'], reverse=True)
@@ -51,7 +51,32 @@ async def get_stu_li():
 
 if __name__ == '__main__':
     async def main():
-        print(await get_stu_li())
+        import jinja2
+        from util import format_timestamp
+        from datetime import datetime
+        from pathlib import Path
+
+        ret = await get_calender()
+
+        for i in ret:
+            begin = i["begin_at"]
+            end = i["end_at"]
+            i['date'] = (
+                f'{format_timestamp(begin)} ~ {format_timestamp(end)}'
+            )
+
+            time_remain = datetime.fromtimestamp(end) - datetime.now()
+            mm, ss = divmod(time_remain.seconds, 60)
+            hh, mm = divmod(mm, 60)
+            i['dd'] = time_remain.days or 0
+            i['hh'] = hh
+            i['mm'] = mm
+            i['ss'] = ss
+
+        html = jinja2.Template(
+            (Path(__file__).parent / 'res' / 'calender.html.jinja').read_text('utf-8')
+        ).render(info=ret)
+        print(html)
 
 
     asyncio.run(main())
