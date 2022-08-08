970bb694033113b1279b532499fdf13bd8c2ce7f
lgc2333 <lgc2333@126.com>
2022-08-08 09:30:24 +0000

    Use Black to Format Code

--- nonebot_plugin_bawiki/__main__.py
+++ nonebot_plugin_bawiki/__main__.py
@@ -12,7 +12,7 @@ from playwright.async_api import Page
 from .data_source import get_calender, get_stu_li
 from .util import format_timestamp
 
-handler_calender = on_command('ba日程表')
+handler_calender = on_command("ba日程表")
 
 
 @handler_calender.handle()
@@ -20,78 +20,78 @@ async def _(matcher: Matcher):
     try:
         ret = await get_calender()
     except:
-        logger.exception('获取日程表出错')
-        return await matcher.finish('获取日程表出错，请检查后台输出')
+        logger.exception("获取日程表出错")
+        return await matcher.finish("获取日程表出错，请检查后台输出")
 
     if not ret:
-        return await matcher.finish('没有获取到数据')
+        return await matcher.finish("没有获取到数据")
 
     for i in ret:
-        if pic := i['picture']:
-            if (not pic.startswith('https:')) and (not pic.startswith('http:')):
-                i['picture'] = 'https:' + pic
+        if pic := i["picture"]:
+            if (not pic.startswith("https:")) and (not pic.startswith("http:")):
+                i["picture"] = "https:" + pic
 
         begin = i["begin_at"]
         end = i["end_at"]
-        i['date'] = (
-            f'{format_timestamp(begin)} ~ {format_timestamp(end)}'
-        )
+        i["date"] = f"{format_timestamp(begin)} ~ {format_timestamp(end)}"
 
         time_remain = datetime.fromtimestamp(end) - datetime.now()
         mm, ss = divmod(time_remain.seconds, 60)
         hh, mm = divmod(mm, 60)
-        i['dd'] = time_remain.days or 0
-        i['hh'] = hh
-        i['mm'] = mm
-        i['ss'] = ss
+        i["dd"] = time_remain.days or 0
+        i["hh"] = hh
+        i["mm"] = mm
+        i["ss"] = ss
 
     try:
         html = jinja2.Template(
-            (Path(__file__).parent / 'res' / 'calender.html.jinja').read_text('utf-8')
+            (Path(__file__).parent / "res" / "calender.html.jinja").read_text("utf-8")
         ).render(info=ret)
 
         async with get_new_page() as page:  # type:Page
-            await page.set_content(html, wait_until='networkidle')
-            pic = await(await page.query_selector('xpath=//div[@id="calendar-box"]')).screenshot()
+            await page.set_content(html, wait_until="networkidle")
+            pic = await (
+                await page.query_selector('xpath=//div[@id="calendar-box"]')
+            ).screenshot()
     except:
-        logger.exception(f'渲染或截取页面出错')
-        return await matcher.finish(f'渲染或截取页面出错，请检查后台输出')
+        logger.exception(f"渲染或截取页面出错")
+        return await matcher.finish(f"渲染或截取页面出错，请检查后台输出")
 
     await matcher.finish(MessageSegment.image(pic))
 
 
-stu_wiki = on_command('ba学生图鉴')
+stu_wiki = on_command("ba学生图鉴")
 
 
 @stu_wiki.handle()
 async def _(matcher: Matcher, arg: Message = CommandArg()):
     arg = arg.extract_plain_text().strip()
     if not arg:
-        return await matcher.finish('请提供学生名称')
+        return await matcher.finish("请提供学生名称")
 
     try:
         ret = await get_stu_li()
     except:
-        logger.exception('获取学生列表出错')
-        return await matcher.finish('获取学生列表表出错，请检查后台输出')
+        logger.exception("获取学生列表出错")
+        return await matcher.finish("获取学生列表表出错，请检查后台输出")
 
     if not ret:
-        return await matcher.finish('没有获取到学生列表数据')
+        return await matcher.finish("没有获取到学生列表数据")
 
     if not (sid := ret.get(arg)):
-        return await matcher.finish('未找到该学生')
+        return await matcher.finish("未找到该学生")
 
-    url = f'https://ba.gamekee.com/{sid}.html'
-    await matcher.send(f'请稍等，正在截取Wiki页面……\n{url}')
+    url = f"https://ba.gamekee.com/{sid}.html"
+    await matcher.send(f"请稍等，正在截取Wiki页面……\n{url}")
 
     try:
         async with get_new_page() as page:  # type:Page
-            await page.goto(url, wait_until='networkidle', timeout=60)
+            await page.goto(url, wait_until="networkidle", timeout=60)
 
             # 删掉header
             await page.add_script_tag(
                 content='document.getElementsByClassName("wiki-header")'
-                        '.forEach((v)=>{v.remove()})'
+                ".forEach((v)=>{v.remove()})"
             )
 
             # 展开折叠的语音
@@ -104,11 +104,11 @@ async def _(matcher: Matcher, arg: Message = CommandArg()):
                 except:
                     pass
 
-            img = await (await page.query_selector(
-                'xpath=//div[@class="wiki-detail-body"]'
-            )).screenshot()
+            img = await (
+                await page.query_selector('xpath=//div[@class="wiki-detail-body"]')
+            ).screenshot()
     except:
-        logger.exception(f'截取wiki页面出错 {url}')
-        return await matcher.finish(f'截取页面出错，请检查后台输出')
+        logger.exception(f"截取wiki页面出错 {url}")
+        return await matcher.finish(f"截取页面出错，请检查后台输出")
 
     await matcher.finish(MessageSegment.image(img))
--- nonebot_plugin_bawiki/data_source.py
+++ nonebot_plugin_bawiki/data_source.py
@@ -7,49 +7,45 @@ from aiohttp import ClientSession
 async def game_kee_request(url, **kwargs):
     async with ClientSession() as s:
         async with s.get(
-                url,
-                headers={
-                    'game-id'   : '0',
-                    'game-alias': 'ba'
-                },
-                **kwargs
+            url, headers={"game-id": "0", "game-alias": "ba"}, **kwargs
         ) as r:
             ret = await r.json()
-            if not ret['code'] == 0:
-                raise ConnectionError(ret['msg'])
-            return ret['data']
+            if not ret["code"] == 0:
+                raise ConnectionError(ret["msg"])
+            return ret["data"]
 
 
 async def get_calender():
-    ret = await game_kee_request('https://ba.gamekee.com/v1/wiki/index')
+    ret = await game_kee_request("https://ba.gamekee.com/v1/wiki/index")
 
     for i in ret:
-        if i['module']['id'] == 12:
-            li: list = i['list']
+        if i["module"]["id"] == 12:
+            li: list = i["list"]
 
             now = time.time()
-            li = [x for x in li if (x['end_at'] >= now >= x['begin_at'])]
+            li = [x for x in li if (x["end_at"] >= now >= x["begin_at"])]
 
-            li.sort(key=lambda x: x['end_at'])
-            li.sort(key=lambda x: x['importance'], reverse=True)
+            li.sort(key=lambda x: x["end_at"])
+            li.sort(key=lambda x: x["importance"], reverse=True)
             return li
 
 
 async def get_stu_li():
-    ret = await game_kee_request('https://ba.gamekee.com/v1/wiki/entry')
+    ret = await game_kee_request("https://ba.gamekee.com/v1/wiki/entry")
 
     li = None
-    for i in ret['entry_list']:
-        if i['id'] == 23941:
+    for i in ret["entry_list"]:
+        if i["id"] == 23941:
 
-            for ii in i['child']:
-                if ii['id'] == 49443:
-                    li = ii['child']
+            for ii in i["child"]:
+                if ii["id"] == 49443:
+                    li = ii["child"]
 
-    return {x['name']: x['content_id'] for x in li}
+    return {x["name"]: x["content_id"] for x in li}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
     async def main():
         import jinja2
         from util import format_timestamp
@@ -61,22 +57,19 @@ if __name__ == '__main__':
         for i in ret:
             begin = i["begin_at"]
             end = i["end_at"]
-            i['date'] = (
-                f'{format_timestamp(begin)} ~ {format_timestamp(end)}'
-            )
+            i["date"] = f"{format_timestamp(begin)} ~ {format_timestamp(end)}"
 
             time_remain = datetime.fromtimestamp(end) - datetime.now()
             mm, ss = divmod(time_remain.seconds, 60)
             hh, mm = divmod(mm, 60)
-            i['dd'] = time_remain.days or 0
-            i['hh'] = hh
-            i['mm'] = mm
-            i['ss'] = ss
+            i["dd"] = time_remain.days or 0
+            i["hh"] = hh
+            i["mm"] = mm
+            i["ss"] = ss
 
         html = jinja2.Template(
-            (Path(__file__).parent / 'res' / 'calender.html.jinja').read_text('utf-8')
+            (Path(__file__).parent / "res" / "calender.html.jinja").read_text("utf-8")
         ).render(info=ret)
         print(html)
-
 
     asyncio.run(main())
