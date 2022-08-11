7f36325209b48f9adf9a0b8df5360eaa7e1a005f
lgc2333 <lgc2333@126.com>
2022-08-11 17:08:52 +0000

    style: use black to format code

--- nonebot_plugin_bawiki/data_source.py
+++ nonebot_plugin_bawiki/data_source.py
@@ -15,7 +15,7 @@ from .util import format_timestamp
 async def game_kee_request(url, **kwargs):
     async with ClientSession() as s:
         async with s.get(
-                url, headers={"game-id": "0", "game-alias": "ba"}, **kwargs
+            url, headers={"game-id": "0", "game-alias": "ba"}, **kwargs
         ) as r:
             ret = await r.json()
             if not ret["code"] == 0:
@@ -63,7 +63,7 @@ async def get_game_kee_page(url):
         # 删掉header
         await page.add_script_tag(
             content='document.getElementsByClassName("wiki-header")'
-                    ".forEach((v)=>{v.remove()})"
+            ".forEach((v)=>{v.remove()})"
         )
 
         # 展开折叠的语音
@@ -157,6 +157,5 @@ if __name__ == "__main__":
             (Path(__file__).parent / "res" / "calender.html.jinja").read_text("utf-8")
         ).render(info=ret)
         print(html)
-
 
     asyncio.run(main())
