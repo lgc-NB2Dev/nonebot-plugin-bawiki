3dcf49366f1dbba93ec4bf433e00247d0f44d0fa
student_2333 <lgc2333@126.com>
2022-08-12 00:40:35 +0800

    bump change: 部分菜单介绍

--- nonebot_plugin_bawiki/__init__.py
+++ nonebot_plugin_bawiki/__init__.py
@@ -2,11 +2,11 @@ from nonebot.plugin import PluginMetadata
 
 from .__main__ import *  # type:ignore
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __plugin_meta__ = PluginMetadata(
     name="BAWiki",
     description="碧蓝档案Wiki插件",
-    usage="",
+    usage="见下表",
     extra={
         "menu_data": [
             {
@@ -21,8 +21,8 @@ __plugin_meta__ = PluginMetadata(
                 "trigger_method": "指令",
                 "trigger_condition": "ba学生图鉴",
                 "brief_des": "查询学生详情",
-                "detail_des": "访问对应学生Wiki页面并截图\n"
-                "指令示例：<ft color=(238,120,0)>ba学生图鉴 白子</ft>",
+                "detail_des": "访问对应学生Wiki页面并截图，支持部分学生别名\n"
+                "指令示例：<ft color=(238,120,0)>ba学生图鉴 白子</ft>；<ft color=(238,120,0)>ba学生图鉴 xcw</ft>",
             },
             {
                 "func": "新学生导航",
